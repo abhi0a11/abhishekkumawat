91fc4de4519de1305e503eaccaaea7beb666d670
Abhishek kumawat <abhikumardambiwal@gmail.com>
2024-07-02 23:23:45 +0530

    backend fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "./index.css";
-export const server = "https://portfolio-backend-8y5h.onrender.com";
+export const server = "https://portfolio-backend-t5t5.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
