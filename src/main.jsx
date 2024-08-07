249626a53d90869d34174a8e1bc904da02f1b95a
Abhishek kumawat <abhikumardambiwal@gmail.com>
2024-08-07 21:51:24 +0530

    backend update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import App from "./App.jsx";
 import "./index.css";
 import "animate.css/animate.compat.css";
 
-export const server = "https://portfolio-backend-t5t5.onrender.com";
+// export const server = "https://portfolio-backend-t5t5.onrender.com";
+export const server = "https://portfolio-backend-new-jfca.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
