b8d1684aef80ca0eeecf357bdfc35496d19bdfc8
Abhishek kumawat <abhikumardambiwal@gmail.com>
2024-09-04 15:22:56 +0530

    vackend update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import "./index.css";
 import "animate.css/animate.compat.css";
 
 // export const server = "https://portfolio-backend-t5t5.onrender.com";
-export const server = "https://portfolio-backend-new-jfca.onrender.com";
+// export const server = "https://portfolio-backend-new-jfca.onrender.com";
+export const server = "https://portfolio-backend-sutv.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
